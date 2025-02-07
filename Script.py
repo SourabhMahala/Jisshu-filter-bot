2c42e0e710af2b9d9ffc496979d410cfd79da020
SourabhMahala <jaatsourabh654@gmail.com>
2025-02-08 05:09:03 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -115,7 +115,7 @@ User - {}"""
 ⭐ Rating: {rating} / 10</b>
 """
 
-    FILE_CAPTION = """<b>{file_name}\n\n𝖩𝗈𝗂𝗇➥ 「<a href="https://t.me/Movies_Kingston">Movies_Kingston</a>」</b>"""
+    FILE_CAPTION = """<b>{file_name}
     
 
     ALRT_TXT = """ᴊᴀʟᴅɪ ʏᴇʜᴀ sᴇ ʜᴀᴛᴏ !"""
